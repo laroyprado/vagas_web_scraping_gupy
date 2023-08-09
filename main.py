adfcbfd1dcbe18ededd417fe1506863219ac8f1e
Laroy Prado <70269831+laroyprado@users.noreply.github.com>
2023-08-09 00:09:55 -0300

    Novas colunas

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ data = response.json()
 job_list = data["data"]
 career_page_urls = []
 
+contador_empresas = 0
+
+
+
+
 for item in job_list:
 
     empresa_site = item["careerPageUrl"].split('/')
@@ -26,26 +31,36 @@ lista_vagas= []
 for empresa in career_page_urls:
     response = requests.get(empresa)
 
-    content = response.content
 
+
+    content = response.content
     site = BeautifulSoup(content, "html.parser")
+    titulo_pagina = site.title.text
 
     vagas = site.findAll("li", attrs={"class": "sc-cc6aad61-3 iBlSMP"})
 
     for vaga in vagas:
+
         corpo_da_vaga = vaga.find("div", attrs={"class": "sc-cc6aad61-4 fFfOku"})
-
+        link_tag = vaga.find("a", attrs={"data-testid": "job-list__listitem-href"})
         corpo_titulo_vaga = corpo_da_vaga.find("div", attrs={"class": "sc-cc6aad61-5 PaHqX"})
+
         if corpo_titulo_vaga:
+            contador_empresas += 1
             nome_vaga = corpo_titulo_vaga.text
-            lista_vagas.append(nome_vaga)
+            lista_vagas.append([nome_vaga,titulo_pagina])
 
 
 
 
 
+todas_as_vagas = pd.DataFrame(lista_vagas,columns=["Título","Empresa"])
 
 
+empresas_registradas = pd.read_excel("vagas.xlsx")
+todas_as_vagas = pd.concat([empresas_registradas, todas_as_vagas])
 
-todas_as_vagas = pd.DataFrame(lista_vagas,columns=["Título"])
 todas_as_vagas.to_excel("vagas.xlsx",index=False)
+
+
+print(" Foram contabilizado" ,contador_empresas,"Empresas")
